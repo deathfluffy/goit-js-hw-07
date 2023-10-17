08b2dc2255d14d884fc698ec91cae04a16decad5
deathfluffy <mrandreu454@gmail.com>
2023-10-17 23:34:45 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,5 @@
 import { galleryItems } from "./gallery-items.js";
-// Change code below this line
+
 const gallerySet = document.querySelector(".gallery");
 const galleryBox = createGallery(galleryItems);
 
